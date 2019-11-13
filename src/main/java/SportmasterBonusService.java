27fc6a9a9ddef5f408062db3d3da50a11826bcd4
NickPro <nickprovidohin@gmail.com>
2019-11-14 00:34:39 +0300

    Fixes #2

--- src/main/java/SportmasterBonusService.java
+++ src/main/java/SportmasterBonusService.java
@@ -1,27 +1,28 @@
 public class SportmasterBonusService {
     public int calculateBonus(int allPurchases, int purchase) {
-        int blueBonusCard = 50;
-        int silverBonusCard = 70;
-        int goldBonusCard = 100;
-        int minPurchaseBonus = 1_000;
+        if (purchase<1000) {
+            return 0;
+        }
 
         int minBlueAllPurchase = 1;
         int maxBlueAllPurchase = 15_000;
         int minSilverAllPurchase = 15_001;
         int maxSilverAllPurchase = 150_000;
-        int bonus = 0;
-        if (purchase<1000){
-            return bonus;
+        int minPurchaseBonus = 1_000;
+
+        if (allPurchases<minBlueAllPurchase) {
+            return 0;
         }
+        int blueBonusCard = 50;
+        int silverBonusCard = 70;
+        int goldBonusCard = 100;
+
         if (minBlueAllPurchase <= allPurchases && allPurchases <= maxBlueAllPurchase) {
-            bonus= purchase / minPurchaseBonus * blueBonusCard;
+            return purchase / minPurchaseBonus * blueBonusCard;
         }
-        else if (minSilverAllPurchase <= allPurchases && allPurchases <= maxSilverAllPurchase){
-            bonus= purchase / minPurchaseBonus * silverBonusCard;
+        if (minSilverAllPurchase <= allPurchases && allPurchases <= maxSilverAllPurchase){
+            return purchase / minPurchaseBonus * silverBonusCard;
         }
-        else if (allPurchases > maxSilverAllPurchase){
-            bonus= purchase / minPurchaseBonus * goldBonusCard;
-        }
-        return bonus;
+        return purchase / minPurchaseBonus * goldBonusCard;
     }
 }
